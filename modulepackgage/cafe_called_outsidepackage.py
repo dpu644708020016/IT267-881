39bafa8141a905ebe71b73cd8c08f87a48111be6
dpu644708020016 <644708020016@dpu.ac.th>
2022-06-26 13:04:54 +0700

    ีupdate module

--- modulepackgage/cafe_called_outsidepackage.py
+++ modulepackgage/cafe_called_outsidepackage.py
@@ -1,9 +1,6 @@
 # #วิธีที่ 1
-from cafe_module import Desserts
-from cafe_module import Drinks
-
-desserts = Desserts()
-print(desserts.show_desserts())
+from cafe.cafe_module import Desserts
+from cafe.cafe_module import Drinks
 
 #วิธีที่ 2
 # from cafe_module import Desserts, Drinks
