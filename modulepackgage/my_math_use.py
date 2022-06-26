988c1adccfd13c2a514a905719642f10b9a2a338
dpu644708020016 <644708020016@dpu.ac.th>
2022-06-26 09:58:33 +0700

    ๊ีupdate modlue

--- modulepackgage/my_math_use.py
+++ modulepackgage/my_math_use.py
@@ -5,7 +5,7 @@ print(f'sqrt of 5 ={sqrt(5)}')
 print(f'circle area = {circle_area(2):,.2f}')
 
 #วิธีที่ 2
-import my_math 
+import my_math as my #ถ้าใส่ชื่อเล่นต้องใส่ทุกฟังชั่น
 print("****วิธีที่ 2****")
-print(f'sqrt of 5 ={sqrt(5)}')
-print(f'circle area = {circle_area(2):,.2f}')
+print(f'sqrt of 5 = {my.sqrt(5)}')
+print(f'circle area = {my.circle_area(2):,.2f}')
