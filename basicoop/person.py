42f6168b13658668a2edc9c344b213dd8e7092c6
dpu644708020016 <644708020016@dpu.ac.th>
2022-06-26 09:52:59 +0700

    Update modlue

--- basicoop/person.py
+++ basicoop/person.py
@@ -3,22 +3,19 @@ class Person:
         self.name = name
         self.sex = sex
         self.profession = profession
-        self.study = 0
+        self.study = 0 #สร้างตัวแปรเพิ่มใส่ 0
 
     def work(self):
-        print(f'{self.name:} working as a {self.profession}')
+        print(f'{self.name:} working as a {self.profession}') #ต้องการปริ้นชื่อตามชื่ออาชีพ ใส่fคือการเอาตัวแปรใส่เข้าไปใน str
 
     def study(self):
         self.study = hours
 
     def show(self):
         print(f'Name:{self.name } Sex: {self.sex} Profession: {self.profession} Study: {self.study}')
-    def show(self):
-        print(f'Name:{self.name} Sex: {self.sex} Profession: {self.profession} Study: {self.study}')
-    def show(self):
-        print(f'Name:{self.name} Sex: {self.sex} Profession: {self.profession} Study: {self.study}')
+
 #person1
-Jassa = Person('Jassa','Female','Softwere Enhiner')
+Jassa = Person('Jassa','Female','Softwere Enhineer')
 Jassa.work()
 Jassa.study=10
 Jassa.show()
