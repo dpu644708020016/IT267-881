7f9cfddaff1bd28137b1182a23ff352f817b3b94
dpu644708020016 <644708020016@dpu.ac.th>
2022-06-26 16:16:37 +0700

    update module

---
+++ modulepackgage/employee/empmkt.py
@@ -0,0 +1,22 @@
+from employee import Employee
+
+class EmpMKT(Employee):
+    def __init__(self,id, name, salary)-> None:
+        super().__init__(id, name, salary)
+        self.location = 'Bangkok'
+        self.commission = 30
+        self.department = 'Marketing'
+
+    def add_location(self,location:str):
+        self.location = location
+
+    def add_commission(self,commission:int):
+        self.commission = com
+
+    def add_emp_detail(self):
+        super().emp_detail()
+        print (f'location: {self.location}')
+        print(f'commission: {self.commission}%')
+
+    def mkt_salary(self):
+        self.emp_salary()
